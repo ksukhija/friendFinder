26ae620ff2c03a7cbd0cfba124feff48648aa8f0
unknown <kamal.sukhija@gmail.com>
2018-12-20 18:10:20 -0500

    added gmail api

--- server.js
+++ server.js
@@ -2,46 +2,131 @@
 // ===========================================================
 var express = require("express");
 var path = require("path");
+const fs = require('fs');
+const {google} = require ('googleapis');
+
 
 var app = express();
+app.use(express.static('app/public'))
 
 var PORT = process.env.PORT || 3000;
 
-// Data
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-}, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Knight",
-  age: 60,
-  forcePoints: 1350
-}];
+// read and verify the credentials
+var rawData = fs.readFileSync('oauth2.keys.json');
+var credentials = JSON.parse(rawData);
+
+const {client_secret, client_id, redirect_uris} = credentials.web;
+console.log(client_id);
+console.log(client_secret);
+console.log(redirect_uris[0]);
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+
+authorize(listLabels);
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(callback) {
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+
+
+  /**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.labels.list({
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = res.data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+}
 
 // Routes
 // ===========================================================
 app.get("/", function(req, res) {
     var fullPath = path.join(__dirname, "app/public/home.html");
+
+
     //console.log(fullPath);
    res.sendFile(fullPath);
 });
 
-app.get("/:character", function(req, res) {
-  var chosen = req.params.character;
+app.get("/:authenticate", function(req, res) {
+  var chosen = req.params;
 
   // What does this log?
   console.log(chosen);
+  //console.log(req.baseUrl);
+  //console.log(req.method)
+  
+  //var msg;
 
+  //console.info(msg, req);
+
+
+ // var myreq = JSON.stringify(req);
   res.end();
 });
 
